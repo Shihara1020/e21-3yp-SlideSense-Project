6c16d59d9647b4d1e1014edbb01e6540664a11a9
Shihara Dewagedara <153976318+Shihara1020@users.noreply.github.com>
2026-04-23 13:20:10 +0530

    Simplify Hall Effect Sensor sample code
    Refactor code to use a single sensor pin and remove unused variables.

--- ESP32_Scripts/Hall Effect Sensor/Sample_Code.cpp
+++ ESP32_Scripts/Hall Effect Sensor/Sample_Code.cpp
@@ -1,35 +1,12 @@
-#include <Arduino.h>
-
-#define SENSOR_PIN 21
-#define LED_BUILTIN 2
-
-int tipCount = 0;
+const int sensorPin = 14;
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(SENSOR_PIN, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  Serial.println("ESP32 Hall Sensor Started");
+  pinMode(sensorPin, INPUT);
 }
 
 void loop() {
-
-  int state = digitalRead(SENSOR_PIN);
-
-  if (state == LOW) {
-    tipCount++;
-
-    Serial.print("Bucket Tipped! Count = ");
-    Serial.println(tipCount);
-
-    delay(300);  // prevent double counting
-  }
-
-  // simple blink to show it's running
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);
+  int state = digitalRead(sensorPin);
+  Serial.println(state);
   delay(200);
 }
